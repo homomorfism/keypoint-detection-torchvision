ef9f063985ecfee9d285e2ab55ca510a7f99aa43
s.arslanov <s.arslanov@innopolis.university>
2021-09-22 17:48:31 +0300

    Colab testing

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ def train(cfg: DictConfig):
     model_checkpoint = ModelCheckpoint(dirpath=cfg.logging.weights_path,
                                        save_last=False,
                                        save_top_k=1,
-                                       monitor='val_loss_epoch',
+                                       monitor='val/val_epoch_total_loss',
                                        mode='min',
                                        filename="chess_epoch={epoch:0.2f}_val_loss={val/smooth_l1_loss_epoch}")
     lr_monitor = LearningRateMonitor(logging_interval='step')
@@ -38,7 +38,10 @@ def train(cfg: DictConfig):
         logger=logger,
         callbacks=[model_checkpoint, lr_monitor],
         max_epochs=cfg.model.epochs,
-        gpus=1)
+        gpus=1,
+        limit_train_batches=0.1,
+        limit_val_batches=0.1
+    )
 
     trainer.fit(model, loader.train_dataloader(), loader.val_dataloader())
 
